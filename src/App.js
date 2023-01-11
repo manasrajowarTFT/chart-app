9b8960e764486518cb212757498f9a7c1f577127
rajowar66 <manasrajowar66@gmail.com>
2023-01-11 20:59:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import datasets from "./data.json";
+import LineChart from "./components/LineChart";
 
 function App() {
+  const [userData, setUserData] = useState({
+    labels: datasets.map((data) => data.services),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: datasets.map((data) => data.cost),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
+  // useEffect(()=>{
+  // const data = fetch('url');
+  // setUserData(data);
+  // },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        width: "40rem",
+      }}
+    >
+      <LineChart chartData={userData} />
     </div>
   );
 }
